32a9b1467c600392eb6882175867e7c72032aa4f
tr2512 <hieu251202@gmail.com>
2022-06-07 18:59:01 +0700

    Fix bug in Thread exercise

--- AimsProject/src/hust/soict/dsai/test/cart/CartTest.java
+++ AimsProject/src/hust/soict/dsai/test/cart/CartTest.java
@@ -1,16 +1,17 @@
 package hust.soict.dsai.test.cart;
 import hust.soict.dsai.aims.cart.Cart;
 import hust.soict.dsai.aims.media.DigitalVideoDisc;
+import hust.soict.dsai.aims.media.Media;
 
 public class CartTest {
 
 	public static void main(String[] args) {
 		Cart cart = new Cart();
-		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The lion king", "Animation", "Roger Allers", 87, 19.95f);
+		Media dvd1 = new DigitalVideoDisc("The lion king", "Animation", "Roger Allers", 87, 19.95f);
 		cart.addMedia(dvd1);
-		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+		Media dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
 		cart.addMedia(dvd2);
-		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+		Media dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
 		cart.addMedia(dvd3);
 		cart.sortByTitle();
 		cart.sortByCost();
